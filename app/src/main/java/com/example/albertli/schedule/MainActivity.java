5267d689f477180186026b3d3e0580333ab6cdf3
albert-liyan <albert.liyan@huawei.com>
2018-07-17 00:00:45 +0800

    add fragment and calender

--- app/src/main/java/com/example/albertli/schedule/MainActivity.java
+++ app/src/main/java/com/example/albertli/schedule/MainActivity.java
@@ -1,13 +1,101 @@
 package com.example.albertli.schedule;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.albertli.schedule.fragment.HolidayFragment;
+import com.example.albertli.schedule.fragment.MenstrualFragment;
+import com.example.albertli.schedule.fragment.RefundFragment;
+import com.example.albertli.schedule.fragment.TravelFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //TabLayout
+    private TabLayout mTabLayout;
+    //ViewPager
+    private ViewPager mViewPager;
+    //mTitle
+    private List<String> mTitle;
+    //Fragment
+    private List<Fragment> mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+        initView();
     }
+    private void initData()
+    {
+        mTitle = new ArrayList<>();
+        mTitle.add("假日");
+        mTitle.add("旅途");
+        mTitle.add("生理期");
+        mTitle.add("还款");
+
+        mFragment = new ArrayList<>();
+        mFragment.add(new HolidayFragment());
+        mFragment.add(new TravelFragment());
+        mFragment.add(new MenstrualFragment());
+        mFragment.add(new RefundFragment());
+    }
+
+    private void initView()
+    {
+        mTabLayout = (TabLayout) findViewById(R.id.mTabLayout);
+        mViewPager = (ViewPager) findViewById(R.id.mViewPager);
+
+        //预加载
+        mViewPager.setOffscreenPageLimit(mFragment.size());
+
+        //滑动监听
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                Log.i("Tag","select page:" + position);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        //设置adapter
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragment.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragment.size();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mTitle.get(position);
+            }
+        });
+
+        //绑定
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
 }
---
+++ app/src/main/java/com/example/albertli/schedule/fragment/RefundFragment.java
@@ -0,0 +1,27 @@
+package com.example.albertli.schedule.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.albertli.schedule.R;
+
+/**
+ * Project Name: Schedule
+ * Details: TODO
+ * Created by albert.li on 2018/7/15.
+ */
+
+
+public class RefundFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_refund, null);
+        return view;
+    }
+}
